0a1e09d4462fc23b8ad462c6f31046f25fb950b2
lynnemunini <muninilynne65@gmail.com>
2023-08-22 23:11:27 +0300

    diary entry form layout

--- app/src/main/java/com/grayseal/traveldiaryapp/ui/main/view/DiaryActivity.kt
+++ app/src/main/java/com/grayseal/traveldiaryapp/ui/main/view/DiaryActivity.kt
@@ -1,6 +1,12 @@
 package com.grayseal.traveldiaryapp.ui.main.view
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.grayseal.traveldiaryapp.R
 
-class DiaryActivity: AppCompatActivity() {
+class DiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_layout)
+    }
 }
--- app/src/main/java/com/grayseal/traveldiaryapp/ui/main/view/MainDashboardActivity.kt
+++ app/src/main/java/com/grayseal/traveldiaryapp/ui/main/view/MainDashboardActivity.kt
@@ -1,12 +1,25 @@
 package com.grayseal.traveldiaryapp.ui.main.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.grayseal.traveldiaryapp.R
 
 class MainDashboardActivity : AppCompatActivity() {
+    private lateinit var addDiaryEntryFab: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeResources()
+
+    }
+
+    private fun initializeResources() {
+        addDiaryEntryFab = findViewById(R.id.add_entry_fab)
+        addDiaryEntryFab.setOnClickListener {
+            startActivity(Intent(this@MainDashboardActivity, DiaryActivity::class.java))
+            finish()
+        }
     }
 }
